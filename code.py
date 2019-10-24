b69e5071683c10d612410d5d6ab54d31a0c5ea68
vitornascimentto <vitornascimentto0@gmail.com>
2019-10-24 16:22:48 -0300

    Initial Code

---
+++ code.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet
+
+description = input('Description: ')
+password = input('Password: ').encode('utf-8')
+
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b'%s'%password)
+
+key = key.decode('utf-8')
+token = token.decode('utf-8')
